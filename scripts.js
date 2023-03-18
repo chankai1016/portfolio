423f6663bff29b75850adac98cae0a58227e6db0
chankai1016 <chankai1016@icloud.com>
2023-03-19 01:03:18 +0800

    update

--- scripts.js
+++ scripts.js
@@ -1,42 +1,54 @@
 // 1. スムーズスクロール
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-  
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
     });
-  });
-  
-  // 2. ハンバーガーメニュー
-  const hamburger = document.querySelector('.hamburger');
-  const navMenu = document.querySelector('.nav-menu');
-  
-  hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    navMenu.classList.toggle('active');
-  });
-  
-  // 3. 画像ギャラリー
-  // 画像ギャラリーの実装は、利用するライブラリやプラグインによって異なります。
-  // 一般的な方法は、JavaScriptプラグインを使用することです。
-  // 例: Lightbox (https://lokeshdhakar.com/projects/lightbox2/)
-  
-  // 4. ダークモード切り替え
-  const darkModeToggle = document.querySelector('#dark-mode-toggle');
-  
-  darkModeToggle.addEventListener('click', () => {
+});
+
+////////////////////////////////////////
+// 2. ハンバーガーメニュー
+////////////////////////////////////////
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const hamburgerIcon = document.querySelector('.hamburger-icon');
+const nav = document.querySelector('nav');
+
+hamburgerMenu.addEventListener('click', () => {
+  nav.classList.toggle('active');
+  hamburgerIcon.classList.toggle('active');
+});
+
+// このコードは、ハンバーガーメニューがクリックされたときに、ナビゲーションとハンバーガーアイコンにactiveクラスを追加/削除します。最後に、styles.cssファイルにactiveクラスに対応するスタイルを追加して、ナビゲーションが表示されるようにしましょう。
+
+
+// 3. 画像ギャラリー
+// 画像ギャラリーの実装は、利用するライブラリやプラグインによって異なります。
+// 一般的な方法は、JavaScriptプラグインを使用することです。
+// 例: Lightbox (https://lokeshdhakar.com/projects/lightbox2/)
+
+// 4. ダークモード切り替え
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+
+darkModeToggle.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
-  });
-  
-  // 5. スクロールアニメーション
-  // スクロールアニメーションの実装は、AOS (https://michalsnik.github.io/aos/) などのライブラリを利用するのが一般的です。
-  
-  // 6. 言語切り替え機能の実装
-  const languageSelector = document.querySelector('.language-selector');
-  
-  languageSelector.addEventListener('change', (e) => {
+});
+
+// 5. スクロールアニメーション
+// スクロールアニメーションの実装は、AOS (https://michalsnik.github.io/aos/) などのライブラリを利用するのが一般的です。
+
+// 6. 言語切り替え機能の実装
+const languageSelector = document.querySelector('.language-selector');
+
+languageSelector.addEventListener('change', (e) => {
     window.location.href = e.target.value;
+});
+
+
+// 言語選択プルダウン
+document.getElementById("language-switcher").addEventListener("change", function () {
+    window.location.href = this.value;
   });
   
